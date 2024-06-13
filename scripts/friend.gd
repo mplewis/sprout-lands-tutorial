9782331dcbc9ecbeb84f5aa68086d7988601941e
Matt Lewis <matt@mplewis.com>
2024-06-13 00:21:52 -0600

    feat: dialogic basics

--- scripts/friend.gd
+++ scripts/friend.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@onready var intr_area: InteractionArea = $InteractionArea
+
+
+func _ready():
+	intr_area.interact = Callable(self, "_interact")
+
+
+func _interact():
+	Dialogic.start("demo")
