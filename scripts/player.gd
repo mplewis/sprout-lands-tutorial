fee258642a1568b2e18f1b5abcdcf074e2c87b26
Matt Lewis <matt@mplewis.com>
2024-06-16 17:18:52 -0600

    wip: learning to use collision areas

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 const SPEED := 150.0
 const INERTIA := 0.7
 
-@onready var anim_tree := $AnimationTree
+@onready var player_anim: AnimationTree = $AnimationTree
+@onready var dagger: Node2D = $DaggerPosition/Dagger
+@onready var dagger_anim: AnimationPlayer = $DaggerPosition/Dagger/AnimationPlayer
 @onready
 var state_mach: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 
@@ -14,6 +16,12 @@ var talking := false
 func _ready():
 	Dialogic.timeline_started.connect(func(): talking = true)
 	Dialogic.timeline_ended.connect(func(): talking = false)
+	dagger.visible = false
+
+
+func _input(event):
+	if event.is_action_pressed("ui_attack"):
+		_attack()
 
 
 func _physics_process(_delta):
@@ -30,10 +38,16 @@ func _physics_process(_delta):
 	var moving := velocity.length() > 1
 
 	if inputing:
-		anim_tree.set("parameters/idle/blend_position", in_dir)
-		anim_tree.set("parameters/walk/blend_position", in_dir)
+		player_anim.set("parameters/idle/blend_position", in_dir)
+		player_anim.set("parameters/walk/blend_position", in_dir)
 
 	if moving:
 		state_mach.travel("walk")
 	else:
 		state_mach.travel("idle")
+
+
+func _attack():
+	dagger.visible = true
+	dagger_anim.play("attack")
+	print("attack")
